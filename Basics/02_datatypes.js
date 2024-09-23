c994cb3e38a3b4ae4238e2f6ff6482cc3f941077
Devansh Gupta <55062711+imdevansh@users.noreply.github.com>
2024-09-23 05:58:36 +0000

    datatypefile added

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "devansh"
+let age = 20
+let isloggedIn = false
+let state = null
+//number => 2 to power 53
+//bigInt =
+//string =  true/false
+//null = standalone value which is empty
+//undefine =>
+// symbol => unique
+
+
+//Non primitive datatype
+
+//object
+console.log(typeof(state));//object
+console.log(typeof undefine); // undefine
+

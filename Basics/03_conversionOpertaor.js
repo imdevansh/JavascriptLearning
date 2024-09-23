4b24ab3116dfdbb1b5e8b091316c5dbfa936f806
Devansh Gupta <55062711+imdevansh@users.noreply.github.com>
2024-09-23 06:12:07 +0000

    coversion of dattypes.

---
+++ Basics/03_conversionOpertaor.js
@@ -0,0 +1,26 @@
+let score = "3abnc3";
+//console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+// "33" => 33
+//"33abbd" => NaN
+// true => 1. false => 0
+
+let isloggedIn = "1wa";
+
+let booleanIsLoggedIn = Boolean(isloggedIn);
+console.log(booleanIsLoggedIn);
+//1 => true,0=> false
+//""" => false"
+// "as" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+

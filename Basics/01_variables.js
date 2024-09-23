993e6637e889433bb9564aa8cc3ca8c6a649c743
Devansh Gupta <55062711+imdevansh@users.noreply.github.com>
2024-09-23 05:41:50 +0000

    added variablefile

---
+++ Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accoundId = 123123
+let accountEmail = "devanshgupta7240@gmail.com"
+var accountPassword = "1231"
+
+accountCity = 'New Delhi'
+
+//accoundId = 12 we can cannot change const
+
+accountEmail = "gutpta@gmail.com"
+accountPassword = "dev!12"
+accountCity = "Bengaluru"
+
+console.table([accoundId,accountEmail, accountPassword,accountCity])

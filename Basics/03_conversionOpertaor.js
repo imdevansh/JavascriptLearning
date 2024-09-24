2069e37a2987f851a43b5cd2522347b2b2c50d7b
Devansh Gupta <55062711+imdevansh@users.noreply.github.com>
2024-09-24 07:40:09 +0000

    04 added

--- Basics/03_conversionOpertaor.js
+++ Basics/03_conversionOpertaor.js
@@ -13,7 +13,7 @@ let valueInNumber = Number(score);
 let isloggedIn = "1wa";
 
 let booleanIsLoggedIn = Boolean(isloggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 //1 => true,0=> false
 //""" => false"
 // "as" => true
@@ -21,6 +21,41 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/************** Operations ********************/
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello ";
+let str2 = "Devansh";
+
+let str3 = str1 + str2;
+// console.log(1 + "3");
+// console.log("1" + 3);
+// console.log(1 + 2 + "3");
+// console.log(4 + 2  * 6 %2);
+
+// console.log(+true);
+// console.log(-true);
+// //console.log(true+);
+
+// console.log(+false);
+// console.log(-false);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let x = 3; 
+let k = 3;
+let y = ++x;
+let z = k++;
+console.table([x,k,y,z]);
+
+
+
 

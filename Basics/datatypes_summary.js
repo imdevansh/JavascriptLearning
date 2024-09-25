66852690fd2f123c3066015e1c3272adb484f1f3
Devansh Gupta <55062711+imdevansh@users.noreply.github.com>
2024-09-25 04:21:25 +0000

    added nulldatatype

--- Basics/datatypes_summary.js
+++ Basics/datatypes_summary.js
@@ -8,7 +8,7 @@ const score = 100;
 const scoreValue = 100.3;
 
 const isLoggedIn = false;
-const temp = null;
+const temp = null; // object
 let userEmail;
 
 const id = Symbol('123')// use for unique value
@@ -43,3 +43,5 @@ const myFunction = function(){
 console.log(typeof myFunction); //function
 console.log(typeof obj);// object
 console.log(typeof heroes);// object
+console.log(typeof temp);
+

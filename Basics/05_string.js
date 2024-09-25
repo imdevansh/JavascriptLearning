aa746d8a42ccb5ee728f37aaa67e64960b452822
Devansh Gupta <55062711+imdevansh@users.noreply.github.com>
2024-09-25 04:57:19 +0000

    String added

---
+++ Basics/05_string.js
@@ -0,0 +1,42 @@
+const name = "Devansh";
+const repoCount = 50;
+
+//console.log(name + repoCount + "Values")
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Devansh-Gupta-101")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('a'));
+const newvar = gameName.substring(0,4);
+// console.log((newvar));
+
+const anotherVar = gameName.slice(-10)
+// console.log(anotherVar);
+
+const newString = "     Devansh";
+// console.log(newString);
+// console.log(newString.trim());
+
+const url = "https://devansh.com/devansh%10gupta" ;
+// console.log(url.replace('%10','-'))
+// console.log(url.includes('devansh'));
+
+
+console.log(gameName.split('-',2));
+let varone = 'Devansh';
+console.log(varone.split(""));
+
+
+
+
+
+
+
+
+
+

60a1b1bfd8fbffdf0e1afe5af27c4640dea4d26e
Devansh Gupta <55062711+imdevansh@users.noreply.github.com>
2024-09-26 06:02:37 +0000

    maths done

--- Basics/06_Maths_and_nums.js
+++ Basics/06_Maths_and_nums.js
@@ -6,4 +6,13 @@ console.log(balance);
 
 
 const num = 4.23;
-console.log(Math.ceil(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+// console.log(balance.toPrecision(2));
+
+// console.log(balance.toFixed(2));
+
+const hundreds = 100000000;
+
+console.log(hundreds.toLocaleString('en-in'));//10,00,00,000
+

da8895e715093eb6bad49bcadc3b9e5e1fc573f4
Devansh Gupta <55062711+imdevansh@users.noreply.github.com>
2024-09-27 06:03:21 +0000

    01_array added

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,49 @@
+const myArray = [0, 1, 2, 3, 4, 5, "Devansh"];
+// console.log(1+ +"2"+ "2");
+// console.log(myArray[1], myArray[6]);
+
+const myHeroes = ["Iron man","Witcher"]
+
+//const myArr2 = new Array(1,2,3);
+// console.log(myArr2);
+
+//Array Method.
+// myArray.push(6);
+// console.log(myArray);
+
+// myArray.pop();
+// console.log(myArray);
+
+// myArray.unshift(0);
+// myArray.unshift(9);
+// console.log(myArray);
+// myArray.shift()
+// console.log(myArray);
+
+// console.log(myArray.includes(3));
+// console.log(myArray.indexOf(3));
+ 
+// const newArr = myArray.join();
+// console.log(myArray);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//slice and splice
+
+console.log("A ", myArray);
+const myn1 = myArray.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArray);
+
+const myn2 = myArray.splice(1,3)
+console.log("C ", myArray);
+console.log(myn2);
+
+
+
+
+
+
+
+

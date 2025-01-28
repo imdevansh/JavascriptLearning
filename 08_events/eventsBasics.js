0e6cb8db91a6bc74b1970b1cddc6be0712465f95
Devansh Gupta <55062711+imdevansh@users.noreply.github.com>
2025-01-28 13:02:31 +0000

    remove image

--- 08_events/eventsBasics.js
+++ 08_events/eventsBasics.js
@@ -1,3 +1,7 @@
 // document.getElementById('horse').onclick = function(){
 //     alert("Horse clicked");
 // };
+
+document.getElementById('horse').addEventListener('click',function(){
+    alert("Horse...");
+},false)
